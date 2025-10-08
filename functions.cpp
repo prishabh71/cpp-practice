428cb4aae38b8fa5787a123d6d41be83f150f657
Prishabh Mishra <prishabhmishra@outlook.com>
2025-10-08 20:32:19 +0530

    Add files via upload
    A simple calculator by using 2 functions and switch statement

---
+++ functions.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+float cal(float, float, char);
+float cal(float a, float b, char w )
+{
+    float result;
+    switch(w)
+   {
+    case'+':
+    result = a + b;
+    break;
+    case'-':
+    result = a - b;
+    break;
+    case'*':
+    result = a * b;
+    break;
+    case'/':
+    if(b != 0)
+    {
+    result = a / b;
+    }
+    else
+    {
+    cout<<"Division by zero not allowed"<<endl;
+    return 0;
+    }
+    break;
+    default:
+    cout<<"Invalid operator"<<endl;
+    return 0;
+   }
+    return result;
+}
+int main()
+{
+float num1, num2;
+char x;
+cout<<"Enter an arithmetic operator (+, -, *, /):- "<<endl;
+cin>>x;
+cout<<"Enter 1st number:- "<<endl;
+cin>>num1;
+cout<<"Enter 2nd number:- "<<endl;
+cin>>num2;
+cout<<"The result is:- "<<cal(num1, num2, x)<<endl;
+return 0;
+}
